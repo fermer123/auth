0563e8a6f4da208adf32f7fa36db7e0cbcc57116
fermer123 <fermer_525@mail.ru>
2022-04-19 00:20:15 +0500

    navigate

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const LoginPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>Login</div>
+      <p>
+        Or <Link to='/register'>register</Link>
+      </p>
+    </div>
+  );
 };
 
 export default LoginPage;
---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,14 @@
+import { Link, Navigate } from 'react-router-dom';
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <div>Register</div>
+      <p>
+        Sign in <Link to='/login'>Login</Link>
+      </p>
+    </div>
+  );
+};
+
+export default RegisterPage;
