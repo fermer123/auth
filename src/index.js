50208a73e1ead06610d959489c07512849c27da6
fermer123 <fermer_525@mail.ru>
2022-05-23 22:00:34 +0500

    npm json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,7 +7,8 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import './firebase';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
@@ -15,5 +16,4 @@ ReactDOM.render(
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
